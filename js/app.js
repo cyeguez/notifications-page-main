4398aec686bbd2a823cb13f073a5c8f48240d9db
cyeguez <cesar.yeguez@gmail.com>
2023-10-21 17:34:55 -0300

    feat(Se agrega estilos para que se ajuste a desktop, se agrega en js para las noficaciones)

--- js/app.js
+++ js/app.js
@@ -1,11 +1,41 @@
-const posInd = document.getElementById('positionIdicador');
 
-const allComments= document.querySelectorAll('.wrapper>article');
+const counter = document.querySelector(".header__title--counter"); //ubicación del contador de notificaciones sin leer
+const allComments = document.querySelectorAll(".wrapper article");
+let unRead = document.querySelectorAll("article.active") // Notificaciones  no leídas
+let cont=0;
+
+function updateCounter(value) {
+  let numNotifications = value;
+  counter.innerHTML = numNotifications;
+}
 
 
 
-allComments.forEach((comment)=>{
-  comment.addEventListener('click', function(){
-    posInd.classList.remove("indicador");
+updateCounter(unRead.length);
+
+allComments.forEach(function (notification) {
+  notification.addEventListener("click", function () {
+    
+    if(notification.classList.contains("active")) {
+      cont+=1;    
+      let index= unRead.length;
+      let value = index - cont;
+      console.log(value);
+      notification.classList.remove("active");
+     
+      updateCounter(value);
+      
+    }
+
+
   });
-})
+   
+});
+
+
+
+
+  
+   
+  
+
