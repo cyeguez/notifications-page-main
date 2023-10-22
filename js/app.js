c191ab899069a3725c725466e2778bb18e515f12
cyeguez <cesar.yeguez@gmail.com>
2023-10-22 00:02:00 -0300

    doc(Se elimina el readme por default y se agrega el personalizado)

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 
-const counter = document.querySelector(".header__title--counter"); //ubicación del contador de notificaciones sin leer
-const allComments = document.querySelectorAll(".wrapper article");
-let unRead = document.querySelectorAll("article.active") // Notificaciones  no leídas
-let cont=0;
+const counter = document.querySelector(".header__title--counter"); //ubicación del contador de notificaciones sin leer.
+const allComments = document.querySelectorAll(".wrapper article"); // el contenedor de todas las publicaciones.
+let unRead = document.querySelectorAll("article.active") // Notificaciones  no leídas.
+let cont=0; //Declaracion del contador.
 
-function updateCounter(value) {
+function updateCounter(value) { //Creo la función encargada de actualizar el contador de notificaciones sin leer.
   let numNotifications = value;
-  counter.innerHTML = numNotifications;
+  counter.innerHTML = numNotifications; // Muestro el valor del contador de notificaciones sin leer.
 }
 
 
@@ -17,13 +17,12 @@ allComments.forEach(function (notification) {
   notification.addEventListener("click", function () {
     
     if(notification.classList.contains("active")) {
-      cont+=1;    
-      let index= unRead.length;
-      let value = index - cont;
-      console.log(value);
-      notification.classList.remove("active");
+      cont+=1;    // cuento las veces que presiono las publicaciones sin leer.
+      let index= unRead.length; // Obtengo el número total de notificaciones no leídas.
+      let value = index - cont; //Cálculo el el número de publicaciones sin leer restantes.
+      notification.classList.remove("active"); // Elimino la clase activa al darle click.
      
-      updateCounter(value);
+      updateCounter(value); // Llamo al método updateCounter para actualizar el contador de notificaciones no leídas.
       
     }
 
